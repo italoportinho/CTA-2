6f5494fbd7472813b4df101191b3d47e47a635b8
rodrigo.coelho <rodrigo.coelho@WAPLTRN31V>
2019-04-05 16:37:24 -0300

    Teste project;

---
+++ CTA.Tests/ViewModel/RegisterViewModelTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CTA.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CTA.Tests.ViewModel
+{
+    [TestClass]
+    public class RegisterViewModelTest
+    {
+        [TestMethod]
+        public void ValidaEmail()
+        {
+            RegisterViewModel register = new RegisterViewModel()
+            {
+                Email = "lalala",
+                Password = "teste",
+                ConfirmPassword = "teste"
+            };
+
+            
+        }
+    }
+}
